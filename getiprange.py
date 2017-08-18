88a818f055305ff64097c62449551649d60e286c
lalopez1962@hotmail.com <tcnoviembre2013@gmail.com>
2017-08-19 00:34:48 +0200

    getiprange

--- getiprange.py
+++ getiprange.py
@@ -20,26 +20,4 @@ def getiprange():
 
 if __name__ == "__main__":
     print getiprange()
-=======
-# -*- coding: utf-8 -*-
-# getiprange.py
-""" Hace ping a un conjunto de equipos
-"""
-# 17 de Agosto de 2017
-# Luis Angel Lopez
-# Version 1.0
 
-import socket
-
-
-def getiprange():
-    host_name = socket.gethostname()
-    host_ip = socket.gethostbyname(host_name)
-    a = host_ip.split(".", 3)[0:3]
-    b = ".".join(a) + "."
-    return b
-
-
-if __name__ == "__main__":
-    print getiprange()
->>>>>>> 4bef55164a8691197280ac77a1d1a703914ebcd7
